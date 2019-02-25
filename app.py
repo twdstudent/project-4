b56484f4a68635145ff1e561a5cc170e4f6d9a6b
Thomas Dodson <tw.dodson@yahoo.co.uk>
2019-02-25 15:57:44 +0000

    fixed bug in app.py file

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import env #will import the entire file, and therefore, allow us access to our environmental variables.
+#import env will import the entire file, and therefore, allow us access to our environmental variables.
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
