8daa62c33c6857a8204ab8902d00fc87c5ed90d5
Thomas Dodson <tw.dodson@yahoo.co.uk>
2019-02-25 17:43:42 +0000

    backing to share

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 import os
-#import env will import the entire file, and therefore, allow us access to our environmental variables.
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
-
-'''
-MongoDB stores its data in a JSON like format called BSON.
-so import something from the BSON library as well
-'''
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'recipePage'
@@ -15,7 +9,7 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
-@app.route('/') #'/' refers to the default route.
+@app.route('/') 
 def index():
     return render_template("index.html")
     
@@ -35,6 +29,19 @@ if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True) 
+
+
+#'/' refers to the default route.
+
+'''
+MongoDB stores its data in a JSON like format called BSON.
+so import something from the BSON library as well
+'''
+
 '''
 setting it to true allows the changes to be picked up automatically in the browser.
+
+MongoDB stores its data in a JSON like format called BSON.
+so import something from the BSON library 
 '''
+
